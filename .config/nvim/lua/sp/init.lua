6309949bc6db4b53f1a4d1f504465be2d262c7f2
Sopheak <spk91011@gmail.com>
2023-12-02 08:17:03 +0700

    add nvim config lua

---
+++ .config/nvim/lua/sp/init.lua
@@ -0,0 +1,5 @@
+require("sp.set")
+require("sp.remap")
+
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
---
+++ .config/nvim/lua/sp/remap.lua
@@ -0,0 +1,12 @@
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+local options = { noremap = true }
+vim.keymap.set("i", "kj", "<Esc>", options)
+vim.keymap.set("v", "kj", "<Esc>", options)
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
