be7255b4a942580e37526c0fa5a3914fd6e3103c
sopheak <sopheaksek@vtech.one>
2024-09-11 11:19:22 +0700

    remove some plugins

--- .config/nvim/lua/plugins/others.lua
+++ .config/nvim/lua/plugins/others.lua
@@ -1,15 +1,1 @@
-return {
-    {
-        "stevearc/oil.nvim",
-        ---@module 'oil'
-        ---@type oil.SetupOpts
-        opts = {
-            view_options = {
-                show_hidden = true,
-                is_always_hidden = function(name)
-                    return name == ".git" or name == "node_modules"
-                end,
-            },
-        },
-    },
-}
+return {}
